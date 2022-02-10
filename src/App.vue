8873f42b3c83d030b363f8eba00698ce8ed69684
Max <bazarsadtsogtjargal@gmail.com>
2022-02-10 23:17:31 +0900

    emit tasks

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="container"><Header title="Task Tracker" /></div>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+  </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     };
   },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+  },
+
   created() {
     this.tasks = [
       {
@@ -32,7 +45,7 @@ export default {
         id: 3,
         text: 'Learn Tailwind',
         day: '10 Feb 2022',
-        reminder: true,
+        reminder: false,
       },
     ];
   },
